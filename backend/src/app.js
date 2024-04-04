db48aa0c852c2e53aae1d4669a13c4f46165e5c8
Muhammed Sheikh <mdsheikh9295@gmail.com>
2024-04-04 12:11:53 +0530

    Changed CORS config

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
